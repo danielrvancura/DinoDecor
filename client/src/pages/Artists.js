21406a5d6e7ba1bb9739c72f91766dcc6175d8e6
danielvancura <danielvancura@gmail.com>
2018-03-15 13:38:33 -0700

    1:38pm commit

--- client/src/pages/Artists.js
+++ client/src/pages/Artists.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import caveman1 from '../images/cavemen/caveman1.jpg';
 
 const Artists = () => {
     return (
         <div className='container'>
-            <h1>My Favorite Food</h1>
-            <p>Would have to be Steak. Cliche, I know.</p>
+            <h1>Featured Artists</h1>
+            <div class="row">
+            <div class="col-sm-4">
+            <img class="img" src={caveman1} />
+            </div>
+            <div class="col-sm-8"><p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys
+            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+            leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with
+            the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+            software like Aldus PageMaker including versions of Lorem Ipsum
+            </p>
+            </div>
+            </div>
         </div>
     )
 }
