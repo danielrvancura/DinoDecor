db7bb0716aa75d7ecdeda05dd2b74fa127efcb27
Brett <brettspencr@gmail.com>
2018-03-15 12:39:52 -0700

    Last commit on master

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,9 +14,11 @@ class Home extends Component {
         return (
             <div className='container'>
                 <Carousel />
-                <hr>
+                <hr />
                 <div>
                     <h1>Featured Artist: Sam Caveman</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempor dolor quis ex egestas, vel porta nisi suscipit. Suspendisse eget condimentum odio. Proin ac pharetra eros. Pellentesque laoreet vehicula egestas. Vestibulum aliquet sem vitae purus tempus sagittis. Nullam laoreet lacinia nisl id auctor. Suspendisse sollicitudin pharetra facilisis. Mauris eros orci, ultricies sed risus vel, condimentum hendrerit est.</p>
+                    <p>Donec molestie ac augue non molestie. In fermentum leo et leo pulvinar, sollicitudin varius enim fringilla. Donec posuere, nunc ac convallis posuere, nunc ipsum egestas sapien, ac tempor nisl ante eget lacus. Maecenas quis lectus eget nulla cursus semper in sed mi. Sed posuere vehicula sapien sit amet scelerisque. Donec nec velit ex. Ut ex nisl, cursus at felis quis, blandit elementum leo. Nunc imperdiet lacus sit amet velit faucibus, ac semper metus hendrerit.</p>
                 </div>
             </div>
         )
